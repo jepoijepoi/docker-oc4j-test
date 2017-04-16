545243309fe6db5eff4ca688c8f267ff5446aaec
CPA IT Support <cpaitsupport@calgaryparking.com>
2017-04-16 03:57:09 +0000

    nothing

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 export OC4J_ADMIN_PASSWORD=$(strings /dev/urandom | grep -o '[[:alnum:]]' | head -n 30 | tr -d '\n') 
 sed -i 's/${OC4J_ADMIN_PASSWORD}/'$OC4J_ADMIN_PASSWORD'/g' /oc4j/j2ee/home/config/system-jazn-data.xml
 echo "Default password for oc4jadmin user is $OC4J_ADMIN_PASSWORD. Please change it in the console"
-curl -o /oc4j/j2ee/home/config/data-sources.xml $DS_CONFIG_URL
+wget -O /oc4j/j2ee/home/config/data-sources.xml $DS_CONFIG_URL
 cd /oc4j
 ./bin/oc4j -start
