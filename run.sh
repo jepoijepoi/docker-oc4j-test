28d4bf48e3382f8000190d0bee607b678c7988ca
CPA IT Support <cpaitsupport@calgaryparking.com>
2017-04-18 02:47:08 +0000

    t

--- run.sh
+++ run.sh
@@ -4,5 +4,4 @@ sed -i 's/${OC4J_ADMIN_PASSWORD}/'$OC4J_ADMIN_PASSWORD'/g' /oc4j/j2ee/home/confi
 echo "Default generated password for oc4jadmin user is $OC4J_ADMIN_PASSWORD -  CHANGE IT IN THE OC4J WEB CONSOLE."
 wget -O /oc4j/j2ee/home/config/data-sources.xml $DS_CONFIG_URL
 #wget -O /oc4j/j2ee/home/applications/bits.ear $BITS_APP_URL
-cd /oc4j
 ./bin/oc4j -start
